062206925554769ca18581f33433824279f27c2b
Stanchevvv <122915555+Stanchevvv@users.noreply.github.com>
2024-07-24 22:23:01 +0300

    Footer update

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
     
     </Authprovider>
     </StateProvider>
-     {/* <Footer /> */}
+     <Footer />
 
     </>
     
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,25 +9,25 @@ function Footer() {
           <div className="col-sm-2">
             <h3>Информация</h3>
             <ul>
-              <li><a href="https://vision-shop.bg/about_us">За мен - Независим дистрибутор на Vision</a></li>
-              <li><a href="https://vision-shop.bg/dostavka">Доставка</a></li>
-              <li><a href="https://vision-shop.bg/poveritelnost">Декларация за поверителност</a></li>
-              <li><a href="https://vision-shop.bg/uslovia-za-polzvane">Условия за ползване</a></li>
+              <li><span href="https://vision-shop.bg/about_us">За мен - Независим дистрибутор на Vision</span></li>
+              <li><span href="https://vision-shop.bg/dostavka">Доставка</span></li>
+              <li><span href="https://vision-shop.bg/poveritelnost">Декларация за поверителност</span></li>
+              <li><span href="https://vision-shop.bg/uslovia-za-polzvane">Условия за ползване</span></li>
             </ul>
           </div>
           <div className="col-sm-2">
             <h3>Полезни връзки</h3>
             <ul>
-              <li><a href="https://vision-shop.bg/contact">Контакти</a></li>
-              <li><a href="https://vision-shop.bg/sitemap">Карта на сайта</a></li>
+              <li><span >Контакти</span></li>
+              <li><span >Карта на сайта</span></li>
             </ul>
           </div>
           <div className="col-sm-2">
             <h3>Профил</h3>
             <ul>
-              <li><a href="https://vision-shop.bg/account">Профил</a></li>
-              <li><a href="https://vision-shop.bg/index.php?route=account/order">История на поръчките</a></li>
-              <li><a href="https://vision-shop.bg/index.php?route=account/newsletter">Бюлетин</a></li>
+              <li><span >Профил</span></li>
+              <li><span >История на поръчките</span></li>
+              <li><span >Бюлетин</span></li>
             </ul>
           </div>
           <div className="col-sm-2">
@@ -37,7 +37,7 @@ function Footer() {
                 <i className="fa fa-phone"></i>
               </div>
               <div className="extra-wrap">
-                <div>0886 246 217</div>
+                <div>0999 222 237</div>
               </div>
             </div>
           </div>
